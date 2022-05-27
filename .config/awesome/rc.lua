39a7199067e42e48ce8848f8fc62156f096f8d01
Eric Milks <emmilks@yahoo.com>
2022-05-26 23:26:14 -0400

    Removed launcher from awesomewm bar

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -113,8 +113,8 @@ else
 end
 
 
-mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon,
-                                     menu = mymainmenu })
+--mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon,
+  --                                   menu = mymainmenu })
 
 -- Menubar configuration
 menubar.utils.terminal = terminal -- Set the terminal for applications that require it
