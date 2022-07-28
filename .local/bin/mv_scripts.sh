377a8f79ce4f4a6175e88289bab31f6b2b746da4
emmilks <emmilks@yahoo.com>
2022-07-28 18:50:58 -0400

    shellchecked files

--- .local/bin/mv_scripts.sh
+++ .local/bin/mv_scripts.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cp -v $HOME/.dotfiles/.config/rofi/rofi-power $HOME/.local/bin/
-cp -v $HOME/.dotfiles/.config/rofi/rofi-calc.sh $HOME/.local/bin/
+cp -v "$HOME"/.dotfiles/.config/rofi/rofi-power "$HOME"/.local/bin/
+cp -v "$HOME"/.dotfiles/.config/rofi/rofi-calc.sh "$HOME"/.local/bin/
