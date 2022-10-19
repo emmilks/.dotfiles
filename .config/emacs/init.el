ed14e49ed453522ed2e80b52a172d705222d41c1
emmilks <emmilks@yahoo.com>
2022-10-18 21:30:57 -0400

    made edits to config.org

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -24,11 +24,13 @@
   (gcmh-high-cons-threshold (* 256 1024 1024))
   (gc-cons-percentage 0.2))
 
+(use-package no-littering
+  :straight t)
+
 (defvar em/default-font-size 115)
 (defvar em/default-variable-font-size 115)
 
 (use-package emacs
-  :after doom-themes
   :init
   (set-face-attribute 'default nil :font "Fira Code" :height em/default-font-size :weight 'regular)
   (set-face-attribute 'fixed-pitch nil :font "Fira Code" :height em/default-font-size :weight 'regular)
@@ -36,16 +38,20 @@
   :config
   ;; Prevent main screen from appearing at startup
   (setq inhibit-startup-message t)
+  (global-hl-line-mode)
   ;; Line Numbers
   (column-number-mode)
   (global-display-line-numbers-mode t)
 
   ;; Disable line numbers for some modes
-  (dolist (mode '(term-mode-hook
+  (dolist (mode '(org-mode-hook
+        term-mode-hook
+        info-mode-hook
+        text-mode-hook
         shell-mode-hook
         eshell-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0 ))))
-  (setq use-short-answers t)
+  (defalias 'yes-or-no-p 'y-or-n-p)
   (show-paren-mode t)
   ;; Disable bell
   (setq ring-bell-function 'ignore)
@@ -57,19 +63,26 @@
   (set-terminal-coding-system 'utf-8)
   (set-keyboard-coding-system 'utf-8)
   (global-auto-revert-mode t)    ;; Auto Revert Buffer
-  (setq-default tab-width 4    ;; Tab behavior
-    indent-tabs-mode nil)
+  (setq-default tab-width 4)    ;; Tab behavior
+  (setq-default indent-tabs-mode nil) ;; Spaces instead of tabs because I am not a heathen
   (delete-selection-mode t)    ;; Start writing after deletion
   (setq-default fill-column 80)
   (setq-default frame-title-format '("%b")) ; make window title the buffer name
   ;; Increase the amount of data Emacs reads from the process
   (setq read-process-output-max (* 1024 1024))
+  (setq user-emacs-directory "~/.config/emacs/")
+  (setq custom-file (concat user-emacs-directory "custom.el"))
   :hook
   (before-save-hook . whitespace-cleanup))
+  (load-file custom-file)
 
-(setq user-emacs-directory "~/.config/emacs/")
-(use-package no-littering
-  :straight t)
+(defun em/kill-inner-word ()
+  "Kills the entire word your cursor is in. Equivalent to 'ciw' in vim."
+  (interactive)
+  (forward-char 1)
+  (backward-word)
+  (kill-word 1))
+(global-set-key (kbd "C-c w k") 'em/kill-inner-word)
 
 (defun config-visit ()
   (interactive)
@@ -97,6 +110,23 @@
 
 (global-set-key (kbd "<s-return>") 'ansi-term)
 
+(use-package dired
+  :custom
+  (dired-auto-revert-buffer t)
+  (dired-dwim-target t)
+  (dired-hide-details-hide-symlink-targets nil)
+  (dired-listing-switches "-Aghov --group-directories-first")
+  (dired-kill-when-opening-new-dired-buffer t)
+  (dired-recursive-copies 'always)
+  :hook
+  (dired-mode . auto-revert-mode)
+  (dired-mode . dired-hide-details-mode))
+
+(use-package dot-mode
+  :straight t
+  :config
+  (global-dot-mode t))
+
 ;; This is needed as of Org 9.2
 (require 'org-tempo)
 
@@ -111,10 +141,10 @@
 
 (setq org-confirm-babel-evaluate nil)
 
-(use-package org-auto-tangle
-  :straight t
-  :defer t
-  :hook (org-mode . org-auto-tangle-mode))
+;(use-package org-auto-tangle
+;  :straight t
+;  :defer t
+;  :hook (org-mode . org-auto-tangle-mode))
 
 (use-package rainbow-mode
   :straight t
@@ -137,38 +167,44 @@
   (interactive)
   (kill-buffer (current-buffer)))
 (global-set-key (kbd "C-x k") 'kill-current-buffer)
-(global-set-key (kbd "C-x b") 'ibuffer)
+(global-set-key (kbd "C-x C-b") 'ibuffer)
 
 (use-package async
   :straight t
   :config
   (dired-async-mode 1))
 
-(use-package doom-themes
-    :straight t
-    :config
-  ;; Global settings (defaults)
-  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-        doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-one t)
+;; (use-package doom-themes
+;;     :straight t
+;;     :config
+;;   ;; Global settings (defaults)
+;;   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+;;         doom-themes-enable-italic t) ; if nil, italics is universally disabled
+;;   (load-theme 'doom-one t)
 
-  ;; Enable flashing mode-line on errors
-  (doom-themes-visual-bell-config)
-  ;; Enable custom neotree theme (all-the-icons must be installed!)
-  ;;(doom-themes-neotree-config)
-  ;; Corrects (and improves) org-mode's native fontification.
-  (doom-themes-org-config))
+;;   ;; Enable flashing mode-line on errors
+;;   (doom-themes-visual-bell-config)
+;;   ;; Enable custom neotree theme (all-the-icons must be installed!)
+;;   ;;(doom-themes-neotree-config)
+;;   ;; Corrects (and improves) org-mode's native fontification.
+;;   (doom-themes-org-config))
+(use-package modus-themes
+  :straight t
+  :config
+  (load-theme 'modus-vivendi t))
 
 (use-package all-the-icons
-    :straight t
-    :if (display-graphic-p))
+	:straight t
+	:if (display-graphic-p))
 
 (use-package doom-modeline
   :straight t
   :init (doom-modeline-mode 1))
 
 (use-package ess
-    :straight t)
+    :straight t
+    :config
+    (setq ess-indent-with-fancy-comments nil))
 
 (use-package projectile
   :straight t
@@ -187,8 +223,129 @@
 
 (use-package vertico
   :straight t
+  :bind
+  (:map minibuffer-local-map
+  ("M-h" . backward-kill-word))
   :init
   (vertico-mode))
+
+;; Example configuration for Consult
+(use-package consult
+  ;; Replace bindings. Lazily loaded due by `use-package'.
+  :straight t
+  :bind (;; C-c bindings (mode-specific-map)
+		 ("C-c h" . consult-history)
+		 ("C-c m" . consult-mode-command)
+		 ("C-c k" . consult-kmacro)
+		 ;; C-x bindings (ctl-x-map)
+		 ("C-x M-:" . consult-complex-command)     ;; orig. repeat-complex-command
+		 ("C-x b" . consult-buffer)                ;; orig. switch-to-buffer
+		 ("C-x 4 b" . consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
+		 ("C-x 5 b" . consult-buffer-other-frame)  ;; orig. switch-to-buffer-other-frame
+		 ("C-x r b" . consult-bookmark)            ;; orig. bookmark-jump
+		 ("C-x p b" . consult-project-buffer)      ;; orig. project-switch-to-buffer
+		 ;; Custom M-# bindings for fast register access
+		 ("M-#" . consult-register-load)
+		 ("M-'" . consult-register-store)          ;; orig. abbrev-prefix-mark (unrelated)
+		 ("C-M-#" . consult-register)
+		 ;; Other custom bindings
+		 ("M-y" . consult-yank-pop)                ;; orig. yank-pop
+		 ("<help> a" . consult-apropos)            ;; orig. apropos-command
+		 ;; M-g bindings (goto-map)
+		 ("M-g e" . consult-compile-error)
+		 ("M-g f" . consult-flymake)               ;; Alternative: consult-flycheck
+		 ("M-g g" . consult-goto-line)             ;; orig. goto-line
+		 ("M-g M-g" . consult-goto-line)           ;; orig. goto-line
+		 ("M-g o" . consult-outline)               ;; Alternative: consult-org-heading
+		 ("M-g m" . consult-mark)
+		 ("M-g k" . consult-global-mark)
+		 ("M-g i" . consult-imenu)
+		 ("M-g I" . consult-imenu-multi)
+		 ;; M-s bindings (search-map)
+		 ("M-s d" . consult-find)
+		 ("M-s D" . consult-locate)
+		 ("M-s g" . consult-grep)
+		 ("M-s G" . consult-git-grep)
+		 ("M-s r" . consult-ripgrep)
+		 ("M-s l" . consult-line)
+		 ("M-s L" . consult-line-multi)
+		 ("M-s m" . consult-multi-occur)
+		 ("M-s k" . consult-keep-lines)
+		 ("M-s u" . consult-focus-lines)
+		 ;; Isearch integration
+		 ("M-s e" . consult-isearch-history)
+		 :map isearch-mode-map
+		 ("M-e" . consult-isearch-history)         ;; orig. isearch-edit-string
+		 ("M-s e" . consult-isearch-history)       ;; orig. isearch-edit-string
+		 ("M-s l" . consult-line)                  ;; needed by consult-line to detect isearch
+		 ("M-s L" . consult-line-multi)            ;; needed by consult-line to detect isearch
+		 ;; Minibuffer history
+		 :map minibuffer-local-map
+		 ("M-s" . consult-history)                 ;; orig. next-matching-history-element
+		 ("M-r" . consult-history))                ;; orig. previous-matching-history-element
+
+  ;; Enable automatic preview at point in the *Completions* buffer. This is
+  ;; relevant when you use the default completion UI.
+  :hook (completion-list-mode . consult-preview-at-point-mode)
+
+  ;; The :init configuration is always executed (Not lazy)
+  :init
+
+  ;; Optionally configure the register formatting. This improves the register
+  ;; preview for `consult-register', `consult-register-load',
+  ;; `consult-register-store' and the Emacs built-ins.
+  (setq register-preview-delay 0.5
+		register-preview-function #'consult-register-format)
+
+  ;; Optionally tweak the register preview window.
+  ;; This adds thin lines, sorting and hides the mode line of the window.
+  (advice-add #'register-preview :override #'consult-register-window)
+
+  ;; Use Consult to select xref locations with preview
+  (setq xref-show-xrefs-function #'consult-xref
+		xref-show-definitions-function #'consult-xref)
+
+  ;; Configure other variables and modes in the :config section,
+  ;; after lazily loading the package.
+  :config
+
+  ;; Optionally configure preview. The default value
+  ;; is 'any, such that any key triggers the preview.
+  ;; (setq consult-preview-key 'any)
+  ;; (setq consult-preview-key (kbd "M-."))
+  ;; (setq consult-preview-key (list (kbd "<S-down>") (kbd "<S-up>")))
+  ;; For some commands and buffer sources it is useful to configure the
+  ;; :preview-key on a per-command basis using the `consult-customize' macro.
+  (consult-customize
+   consult-theme
+   :preview-key '(:debounce 0.2 any)
+   consult-ripgrep consult-git-grep consult-grep
+   consult-bookmark consult-recent-file consult-xref
+   consult--source-bookmark consult--source-recent-file
+   consult--source-project-recent-file
+   :preview-key (kbd "M-."))
+
+  ;; Optionally configure the narrowing key.
+  ;; Both < and C-+ work reasonably well.
+  (setq consult-narrow-key "<") ;; (kbd "C-+")
+
+  ;; Optionally make narrowing help available in the minibuffer.
+  ;; You may want to use `embark-prefix-help-command' or which-key instead.
+  ;; (define-key consult-narrow-map (vconcat consult-narrow-key "?") #'consult-narrow-help)
+
+  ;; By default `consult-project-function' uses `project-root' from project.el.
+  ;; Optionally configure a different project root function.
+  ;; There are multiple reasonable alternatives to chose from.
+  ;;;; 1. project.el (the default)
+  ;; (setq consult-project-function #'consult--default-project--function)
+  ;;;; 2. projectile.el (projectile-project-root)
+  ;; (autoload 'projectile-project-root "projectile")
+  ;; (setq consult-project-function (lambda (_) (projectile-project-root)))
+  ;;;; 3. vc.el (vc-root-dir)
+  ;; (setq consult-project-function (lambda (_) (vc-root-dir)))
+  ;;;; 4. locate-dominating-file
+  ;; (setq consult-project-function (lambda (_) (locate-dominating-file "." ".git")))
+)
 
 (use-package savehist
   :straight t
@@ -210,9 +367,85 @@
   :init
   (marginalia-mode))
 
-(use-package company
+;; Auto completion example
+(use-package corfu
   :straight t
+  :custom
+  (corfu-auto t)          ;; Enable auto completion
+  ;; (corfu-separator ?_) ;; Set to orderless separator, if not using space
+  (corfu-cycle t)             ;; Enable cycling for `corfu-next/previous'
+  (corfu-preselect-first nil) ;; Disable candidate preselection
+  (corfu-auto-prefix 2)
+  (corfu-auto-delay 0.2)
+  (corfu-quit-at-boundary 'separator)
+  (corfu-preview-current 'insert)
+  :bind (:map corfu-map
+	  ("M-SPC" . corfu-insert-separator)
+	  ("TAB"     . corfu-next)
+	  ([tab]     . corfu-next)
+	  ("S-TAB"   . corfu-previous)
+	  ([backtab] . corfu-previous)
+	  ("S-<return>" . corfu-insert)
+	  ("RET"     . nil) ;; leave my enter alone!
+	  )
+  ;; Another key binding can be used, such as S-SPC.
+  ;; (:map corfu-map ("M-SPC" . corfu-insert-separator))
+  :init
+  (global-corfu-mode))
+
+(use-package magit
+  :straight t
+  :defer t
+  :bind ("C-x g" . magit-status)
+  :commands (magit-status magit-get-current-branch)
+  :custom
+  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
+
+(use-package flycheck
+  :straight t
+  :init (global-flycheck-mode))
+
+(use-package python
   :config
-  (setq company-idle-delay 0.3)
-  (setq company-minimum-prefix-length 3)
-  (global-company-mode))
+  ;; Remove guess indent python message
+  (setq python-indent-guess-indent-offset-verbose nil))
+
+(use-package blacken
+  :straight t
+  :defer t
+  :hook (python-mode-hook . blacken-mode))
+
+(use-package hide-mode-line
+:straight t
+:defer t
+:hook ((inferior-python-mode-hook . hide-mode-line-mode)
+	   (inferior-ess-r-mode-hook . hide-mode-line-mode)))
+
+(use-package lsp-mode
+  :straight t
+  :defer t
+  :defines (lsp-keymap-prefix lsp-mode-map)
+  :init
+  (setq lsp-keymap-prefix "C-c l")
+  :custom
+  ;; Read the documentation for those variable with `C-h v'.
+  ;; This reduces the visual bloat that LSP sometimes generate.
+  (lsp-eldoc-enable-hover nil)
+  (lsp-signature-auto-activate nil)
+  (lsp-completion-enable t)
+  :hook ((lsp-mode . lsp-enable-which-key-integration))
+  :commands (lsp lsp-deferred))
+
+(use-package lsp-pyright
+  :straight t
+  :defer t
+  :custom
+  (lsp-pyright-disable-language-service nil)
+  (lsp-pyright-disable-organize-imports nil)
+  (lsp-pyright-auto-import-completions t)
+  (lsp-pyright-use-library-code-for-types t)
+  (lsp-completion-enable t)
+  ;; Launches pyright when a python buffer is opened.
+  :hook ((python-mode . (lambda ()
+			  (require 'lsp-pyright)
+			  (lsp-deferred)))))
